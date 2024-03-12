968ac425075229b12075731dd9eb865f476d5df5
FelipeBigarelli <felipebigarelli@hotmail.com>
2024-03-12 16:33:04 -0300

    Melhorando a UX

--- Anotações.md
+++ Anotações.md
@@ -12,3 +12,24 @@
 - Não cumprir a política -> quebra
 
 - Previne roubo de requisições, dados, etc.
+***
+
+## CORS - Cross-Origin Resourse Sharing
+- Se a requisição fere a SOP, passa a ser uma request do tipo CORS
+- Compartilhamento de recursos entre origens cruzadas
+***
+
+## Simple vs Preflighted requests
+#### Simple
+- Precisa ter algumas condições, sendo:
+  - Metodos GET, HEAD e POST (se tem outros, deixa de ser simples)
+  - Únicos headers possíveis: Accept, Accept-Language, Content-Language, Content-Type (application/x-www-form-urlencoded, multiform/form-data, text-plain)
+
+#### Preflight
+- Métodos podem ser "perigosos"
+- Verificar antes de enviar uma requisição se a origem está permitida e o método HTTP ou header está permitido
+- Requisição sempre do tipo OPTIONS
+***
+
+## Async/Await
+- Await: o código abaixo da promise depende da resolução da promise ser executada? Se sim, usar await
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,107 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState, useMemo } from 'react';
 
 import {
-  Container, InputSearchContainer, Header, ListContainer, Card,
+  Container, InputSearchContainer, Header, ListHeader, Card,
 } from './styles';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 
+import Loader from '../../components/Loader';
+
+import delay from '../../utils/delay';
+
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const filteredContacts = useMemo(() => contacts.filter((contact) => (
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )), [contacts, searchTerm]);
+
+  // Usar async/await dentro do useEffect: criar função async dentro
+  useEffect(() => {
+    async function loadContacts() {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch(
+          `http://localhost:3001/contacts?orderBy=${orderBy}`,
+        );
+
+        await delay(500);
+
+        const json = await response.json();
+        setContacts(json);
+      } catch (error) {
+        console.log('erro', error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    loadContacts();
+  }, [orderBy]);
+
+  function handleToggleOrderBy() {
+    setOrderBy(
+      (prevState) => (prevState === 'asc' ? 'desc' : 'asc'),
+    );
+  }
+
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <Container>
+      <Loader $isLoading={isLoading} />
+
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar contato" />
+        <input
+          value={searchTerm}
+          type="text"
+          placeholder="Pesquisar contato"
+          onChange={handleChangeSearchTerm}
+        />
       </InputSearchContainer>
 
       <Header>
-        <strong>3 contatos</strong>
+        <strong>
+          {filteredContacts.length}
+          {filteredContacts.length === 1 ? ' contato' : ' contatos'}
+        </strong>
         <Link to="/new">Novo contato</Link>
       </Header>
 
-      <ListContainer>
-        <header>
-          <button type="button" className="sort-button">
+      {/* Transient props ($prop) é um pattern pra passar props que são explicitamente
+      usadas apenas pelo styled-components e não pela DOM */}
+      {filteredContacts.length > 0 && (
+        <ListHeader $orderBy={orderBy}>
+          <button type="button" onClick={handleToggleOrderBy}>
             <span>Nome</span>
             <img src={arrow} alt="Arrow" />
           </button>
-        </header>
+        </ListHeader>
+      )}
 
-        <Card>
+      {filteredContacts.map((contact) => (
+        <Card key={contact.id}>
           <div className="info">
             <div className="contact-name">
-              <strong>Felipe Bigarelli</strong>
-              <small>instagram</small>
+              <strong>{contact.name}</strong>
+              {contact.category_name && <small>{contact.category_name}</small>}
             </div>
-            <span>felipe@devacademy.com.br</span>
-            <span>(61) 99999-9999</span>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
           </div>
 
           <div className="actions">
-            <Link to="/edit/123">
+            <Link to={`/edit/${contact.id}`}>
               <img src={edit} alt="Edit" />
             </Link>
             <button type="button">
@@ -47,7 +109,7 @@ export default function Home() {
             </button>
           </div>
         </Card>
-      </ListContainer>
+      ))}
     </Container>
   );
 }
